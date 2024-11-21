61a1c80f7e5f964cdf7a9c2820c7e0472547e95e
John Pazarzis <jpazarzis@gmail.com>
2024-11-21 15:20:53 -0500

    Fixes the prompts tests.

--- src/impl/tests/test_promt_maker.py
+++ src/impl/tests/test_promt_maker.py
@@ -7,9 +7,11 @@ import os
 
 import querycrafter.src.impl.chatbot as chatbot
 import querycrafter.src.impl.common as common
-import querycrafter.src.impl.prompts as prompts
+import querycrafter.src.impl.prompt_maker as prompt_maker
 
 _CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+DocType = common.DocType
 
 
 def test_doc_string_for_class():
@@ -24,8 +26,7 @@ def test_doc_string_for_class():
             self.name = name
             self.age = age
     """
-
-    prompt = prompts.MAKE_DOC_STRING_FOR_CLASS + txt
+    prompt = prompt_maker.make_prompt(DocType.CLASS, txt)
     response = chatbot.run_query(prompt)
     print(response)
     retrieved = json.loads(response)
@@ -55,8 +56,7 @@ def test_doc_string_for_function():
             raise ConnectionError
         return m
     """
-
-    prompt = prompts.MAKE_DOC_STRING_FOR_FUNCTION + txt
+    prompt = prompt_maker.make_prompt(DocType.FUNCTION, txt)
     response = chatbot.run_query(prompt)
     retrieved = json.loads(response)
 
@@ -84,7 +84,7 @@ def test_doc_string_for_method_1():
         else:
             print(crowd, "hello")
     """
-    prompt = prompts.MAKE_DOC_STRING_FOR_FUNCTION + txt
+    prompt = prompt_maker.make_prompt(DocType.FUNCTION, txt)
     response = chatbot.run_query(prompt)
     retrieved = json.loads(response)
 
@@ -106,7 +106,7 @@ def test_doc_string_for_method_2():
     def say_hello(self):
         return "OK"
     """
-    prompt = prompts.MAKE_DOC_STRING_FOR_FUNCTION + txt
+    prompt = prompt_maker.make_prompt(DocType.FUNCTION, txt)
     response = chatbot.run_query(prompt)
     retrieved = json.loads(response)
 
@@ -129,65 +129,10 @@ def test_doc_string_for_method_3():
     def say_hello(self):
         pass
     """
-    prompt = prompts.MAKE_DOC_STRING_FOR_FUNCTION + txt
+    prompt = prompt_maker.make_prompt(DocType.FUNCTION, txt)
     response = chatbot.run_query(prompt)
     retrieved = json.loads(response)
 
     assert isinstance(retrieved, dict)
     assert len(retrieved) == 2
     assert "summary" in retrieved
-
-
-def test_convert_func_json_to_doc():
-    """Tests the convert_func_json_to_doc."""
-    doc_as_json = {
-        "summary": "Calculate the maximum profit.",
-        "arguments": [
-            {
-                "arg_name": "prices",
-                "arg_type": "list",
-                "desc": "List of stock prices Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit.  Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere.  Quisque eget imperdiet est."
-            },
-            {
-                "arg_name": "some_other",
-                "arg_type": "list",
-                "desc": "List of stock prices Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit.  Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere.  Quisque eget imperdiet est."
-            }
-        ],
-        "return": {
-            "return_type": "int",
-            "desc": "Maximum profit possible Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit. Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere. Quisque eget imperdiet est. "},
-        "prefixed_spaces": 4,
-        "exceptions": [
-            "ValueError",
-            "ConnectionError"
-        ],
-        "notes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit. Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere. Quisque eget imperdiet est. ",
-    }
-    response = prompts.convert_func_json_to_doc(doc_as_json)
-    print(response)
-    for line in response.split("\n"):
-        if line:
-            assert line.startswith("    ")
-            assert len(line) < 80
-
-
-def test_convert_class_json_to_doc():
-    """Tests the convert_class_json_to_doc."""
-    doc_as_json = {"summary": "Represents a person with a name and age.",
-                   "notes": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit. Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere. Quisque eget imperdiet est. ",
-                   "ivars": [{"arg_name": "name",
-                              "arg_type": "str",
-                              "desc": "The name of the person Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit. Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere. Quisque eget imperdiet est. ."},
-                             {"arg_name": "age",
-                              "arg_type": "int",
-                              "desc": "The age of the person.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ipsum at lectus malesuada scelerisque. Curabitur euismod vestibulum hendrerit. Duis ultricies velit vel volutpat venenatis. Nulla suscipit magna et malesuada pulvinar. Curabitur semper sit amet lectus non suscipit. Curabitur justo ante, varius eget iaculis quis, laoreet vitae ipsum. Donec malesuada metus nec rutrum posuere. Quisque eget imperdiet est. "}],
-                   "cvars": [{"arg_name": "TITLES",
-                              "arg_type": "list",
-                              "desc": "List of title options for a person."}],
-                   "prefixed_spaces": 4}
-    response = prompts.convert_class_json_to_doc(doc_as_json)
-    print(response)
-    for line in response.split("\n"):
-        if line:
-            assert line.startswith("    ")
